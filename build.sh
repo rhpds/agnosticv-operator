98d2c9a23bdcad48dfadac134b5ba8e3f3b71117
Guillaume Coré <gucore@redhat.com>
2020-04-15 15:49:41 +0200

    Add Dockerfile for OCP3 (#32)
    * Add Dockerfile for OCP3
    
    Update build.sh to build both OCP4 and OCP3
    
    * Use latest pip to fix psutil error
    
    Add user to /etc/passwd to fix local docker errors with getpwduid, see
    
    https://github.com/operator-framework/operator-sdk/pull/2469

--- build.sh
+++ build.sh
@@ -2,7 +2,14 @@
 echo -n "Version: "
 read VERSION
 
+set -xeuo pipefail
+
 # Now build the Operator
 operator-sdk build quay.io/gpte-devops-automation/agnosticv-operator:${VERSION}
 
 docker push quay.io/gpte-devops-automation/agnosticv-operator:${VERSION}
+
+# Build ocp3 compatible image
+operator-sdk build --image-build-args "-f build/Dockerfile-ocp3" quay.io/gpte-devops-automation/agnosticv-operator:${VERSION}-ocp3
+
+docker push quay.io/gpte-devops-automation/agnosticv-operator:${VERSION}-ocp3
